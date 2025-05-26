198ab60da65d3fc4f95529a039083c66f2dfba2d
gustavoZuin237 <zuingustavo206@gmail.com>
2025-05-26 00:41:46 -0300

    Refactor: removed Handjet font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,20 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
 
 import PressStart2P from '../assets/fonts/PressStart2P-Regular.ttf';
-import Handjet from '../assets/fonts/Handjet.ttf';
 import PixelifySans from '../assets/fonts/PixelifySans.ttf'
 
 export const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: 'PressStart2P';
         src: url(${PressStart2P}) format('truetype');
-        font-weight: 400;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Handjet';
-        src: url(${Handjet}) format('truetype');
         font-weight: 400;
         font-style: normal;
     }
